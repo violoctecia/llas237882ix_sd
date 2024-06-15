27ef6301d0bfc0628c6014fadd1cc3ee27e7c5c0
sq_dev <sqq.dev@gmail.com>
2024-06-15 21:03:42 +0400

    fix: reviews

--- src/stores/hero.js
+++ src/stores/hero.js
@@ -3,12 +3,12 @@ import { defineStore } from 'pinia'
 export const useHeroStore = defineStore('hero', () => {
   const sub = 'Инвестиционный клуб'
   const title = 'Инвестиции<br> от 100% годовых'
-  const desc = 'Получайте до 8.5% каждый месяц от вклада на протяжении 12 месяцев.'
+  const desc = 'Получайте до 8.5% каждый месяц от вклада.'
   const button = 'Начать инвестировать'
   const info = [
     {value: 'Минимальный вклад<br> 100 000₽', icon: '/src/assets/img/icons/i1.svg'},
     {value: 'Максимальный вклад<br> 20 000 000₽', icon: '/src/assets/img/icons/i2.svg'},
-    {value: 'Договор займа<br> под 100% годовых', icon: '/src/assets/img/icons/i3.svg'},
+    {value: 'Договор доверительного<br> управления под 100% годовых', icon: '/src/assets/img/icons/i3.svg'},
     {value: 'Личная встреча в офисе Москва- сити при вкладе от 500 000₽', icon: '/src/assets/img/icons/i4.svg'},
   ]
 
--- src/stores/reviews.js
+++ src/stores/reviews.js
@@ -6,11 +6,11 @@ export const useReviewsStore = defineStore('reviews', () => {
   const items = [
     {
       name: 'Доганин Алексей',
-      image: ['/src/assets/img/other/img-1.jpg', '/src/assets/img/other/img-1@2x.jpg']
+      image: ['/src/assets/img/other/img-2.jpg', '/src/assets/img/other/img-2@2x.jpg']
     },
     {
       name: 'Доганин Алексей',
-      image: ['/src/assets/img/other/img-1.jpg', '/src/assets/img/other/img-1@2x.jpg']
+      image: ['/src/assets/img/other/img-3.jpg', '/src/assets/img/other/img-3@2x.jpg']
     },
     {
       name: 'Доганин Алексей',
